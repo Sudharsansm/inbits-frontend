0762616ccd35952511b8cec60c47afa2d0e3270f
Sudharsan S M <sudharsanmani423@gmail.com>
2026-09-02 15:54:42 +0530

    Update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart({ server: { entry: "server" } }),
+    nitro(),
     viteReact(),
   ],
 });
